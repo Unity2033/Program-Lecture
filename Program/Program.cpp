76399fa43bc15b567983e04355c7715a3f8717f9
Unity2033 <kileab@naver.com>
2025-09-23 19:46:35 +0900

    (2025.9.23) 예외 처리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,10 +1,19 @@
 ﻿#include "stdafx.h"
+#include "Language.h"
 
 template <typename T>
 bool Same(T left, T right)
 {
 	return left == right;
 }
+
+template<>
+bool Same(Language left, Language right)
+{
+	return left.Name() == right.Name();
+}
+
+
 
 int main()
 {
@@ -13,13 +22,26 @@ int main()
 	// 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수
 	// 있는 기능입니다.
 
-	cout << Same('A', 'A') << endl;
-	cout << Same(5, 10) << endl;
-	cout << Same(17.5f, 20.125f) << endl;
-	cout << Same("Head", "Head") << endl;
+	// cout << Same('A', 'A') << endl;
+	// cout << Same(5, 10) << endl;
+	// cout << Same(17.5f, 20.125f) << endl;
+	// cout << Same("Head", "Head") << endl;
 
 #pragma endregion
 
+#pragma region 템플릿 특수화
+	// 특정 자료형에 대해 다르게 처리하고 싶은 경우
+	// 특정한 자료형만 다른 형식으로 동작시키는 기능입니다.
+
+	// Language programLanguage1;
+	// Language programLanguage2;
+	// 
+	// programLanguage1.Initialize("java");
+	// programLanguage2.Initialize("java script");
+	// 
+	// cout << Same(programLanguage1, programLanguage2) << endl;
+
+#pragma endregion
 
 	return 0;
 }
