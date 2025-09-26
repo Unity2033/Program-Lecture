2a8750aa6859c139c601b4d4da893c0901343316
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-09-26 19:46:55 +0900

    (2025.9.26) 가상 함수 테이블

--- Program/Resource.cpp
+++ Program/Resource.cpp
@@ -6,6 +6,11 @@ Resource::Resource()
 	cout << "Created Resource" << endl;
 }
 
+void Resource::Share(const shared_ptr<Resource> & reference)
+{
+	resource = reference;
+}
+
 Resource::~Resource()
 {
 	cout << "Destroy Resource" << endl;
--- Program/Resource.h
+++ Program/Resource.h
@@ -1,8 +1,14 @@
 #pragma once
 class Resource
 {
+private:
+	shared_ptr<Resource> resource;
+
 public:
 	Resource();
+
+	void Share(const shared_ptr<Resource> & reference);
+
 	~Resource();
 };
 
