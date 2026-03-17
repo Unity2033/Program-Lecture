bccf7deacf0432df38e7d94770ea360acd2afecd
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-03-17 21:43:33 +0900

    (2026.3.17) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -62,6 +62,79 @@ public:
 		}
 	}
 
+	void push_back(T data)
+	{
+		Node * newNode = new Node;
+
+		newNode->data = data;
+
+		newNode->next = nullptr;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+		}
+		else
+		{
+			Node * currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+
+			currentNode->next = newNode;
+		}
+
+		size++;
+	}
+
+	void pop_back()
+	{
+		if (head == nullptr)
+		{
+			cout << "linked list is empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+			Node* previousNode = nullptr;
+
+			if (size == 1)
+			{
+				head = deleteNode->next;
+			}
+			else
+			{
+				while (deleteNode->next != nullptr)
+				{
+					previousNode = deleteNode;
+
+					deleteNode = deleteNode->next;
+				}
+
+				previousNode->next = deleteNode->next;
+			}
+
+			delete deleteNode;
+
+			size--;
+		}
+
+	}
+
+	const bool & empty()
+	{
+		return (head == nullptr);
+	}
+
+	~List()
+	{
+		while (head != nullptr)
+		{
+			pop_front();
+		}
+	}
 };
 
 int main()
@@ -71,8 +144,13 @@ int main()
 	list.push_front(10);
 	list.push_front(5);
 
+	list.push_back(20);
+
 	list.pop_front();
 	list.pop_front();
-	list.pop_front();
+	list.pop_back();
+	list.pop_back();
+
+	cout << list.empty() << endl;
 }
 
