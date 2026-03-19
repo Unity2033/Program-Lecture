78fefb8eaa0e23869699ce1ea0d88bbb19314419
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-03-19 21:44:00 +0900

    (2026.3.19) 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -10,71 +10,16 @@ private:
 	{
 		T data;
 		Node * next;
-		Node * previous;
 	};
 
 	int size;
 	Node * head;
-	Node * tail;
+
 public:
 	List()
 	{
 		size = 0;
 		head = nullptr;
-		tail = nullptr;
-	}
-
-	void push_front(T data)
-	{
-		Node * newNode = new Node;
-
-		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			head->previous = newNode;
-
-			newNode->next = head;
-
-			head = newNode;
-		}
-
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "linked list is empty" << endl;
-		}
-		else
-		{
-			Node * deleteNode = head;
-
-			if (head == tail)
-			{
-				head = nullptr;
-				tail = nullptr;
-			}
-			else
-			{
-				deleteNode->next->previous = nullptr;
-
-				head = head->next;
-			}
-
-			delete deleteNode;
-
-			size--;
-		}
 	}
 
 	void push_back(T data)
@@ -82,53 +27,34 @@ public:
 		Node * newNode = new Node;
 
 		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
 
-		if (tail == nullptr)
+		if (head == nullptr)
 		{
 			head = newNode;
-			tail = newNode;
+
+			newNode->next = head;
 		}
 		else
 		{
-			tail->next = newNode;
+			newNode->next = head->next;
 
-			newNode->previous = tail;
+			head->next = newNode;
 
-			tail = newNode;
+			head = newNode;
 		}
 
 		size++;
 	}
 
-	const bool & empty()
-	{
-		return head == nullptr;
-	}
-
-	~List()
-	{
-		while (head != nullptr)
-		{
-			pop_front();
-		}
-	}
 };
 
 int main()
 {
 	List<int> list;
-
-	list.push_front(10);
-	list.push_front(5);
-
+	
+	list.push_back(10);
 	list.push_back(20);
-	list.push_back(30);
 
-	list.pop_front();
-	list.pop_front();
-	list.pop_front();
 
 }
 
