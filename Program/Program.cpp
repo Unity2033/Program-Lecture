a52faa72b6a1cfb221c31ca9170ea70c9ba37ee3
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-03-26 21:44:03 +0900

    (2026.3.26) 해시 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,8 +2,104 @@
 
 using namespace std;
 
+template <typename T>
+class PriorityQueue
+{	
+private:
+	int index;
+	int capacity;
+
+	T * container;
+public:
+	PriorityQueue()
+	{
+		index = 0;
+		capacity = 0;
+		container = nullptr;
+	}
+
+	void resize(int newSize)
+	{
+		capacity = newSize;
+
+		T * temporary = new T[capacity];
+
+		for (int i = 0; i < capacity; i++)
+		{
+			temporary[i] = NULL;
+		}
+
+		for (int i = 0; i < index; i++)
+		{
+			temporary[i] = container[i];
+		}
+
+		delete [ ] container;
+
+		container = temporary;
+	}
+
+	void push(T data)
+	{
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if (index >= capacity)
+		{
+			resize(capacity * 2);
+		}
+
+		container[index++] = data;
+
+		int child = index - 1;
+		int parent = (child - 1) / 2;
+
+		while (child > 0)
+		{
+			if (container[parent] < container[child])
+			{
+				std::swap(container[parent], container[child]);
+			}
+
+			child = parent;
+
+			parent = (child - 1) / 2;
+		}
+	}
+
+	const bool & empty()
+	{
+		return index <= 0;
+	}
+
+	const int & size()
+	{
+		return index;
+	}
+
+	const T & top()
+	{
+		return container[0];
+	}
+
+	~PriorityQueue()
+	{
+		delete[] container;
+	}
+};
+
 int main()
 {
+	PriorityQueue<int> priorityQueue;
+
+	priorityQueue.push(10);
+	priorityQueue.push(50);
+	priorityQueue.push(70);
+	priorityQueue.push(5);
+	priorityQueue.push(30);
+
+	cout << "Priority Queue is Size : " << priorityQueue.size() << endl;
 
 	return 0;
 }
