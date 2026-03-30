3c50d2e6ee67e53e26f30a52303cb0b4dc188938
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-03-30 21:44:05 +0900

    (2026.3.30) 인접 행렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -46,13 +46,64 @@ public:
 		return (unsigned int)key % capacity;
 	}
 
+	template<>
+	unsigned int hash_function(const char * key)
+	{
+		unsigned int sum = 0;
+
+		for (int i = 0; *key != '\0'; i++)
+		{
+			sum += key[i];
+
+			key = key + 1;
+		}
+
+		return sum % capacity;
+	}
+
+	void insert(KEY key, VALUE value)
+	{
+		// 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = hash_function(key);
+
+		// 새로운 노드를 생성합니다.
+		Node * newNode = new Node;
+
+		newNode->key = key;
+
+		newNode->value = value;
+
+		newNode->next = nullptr;
+
+		// 노드가 1개라도 존재하지 않는다면
+		if (bucket[hashIndex].count == 0)
+		{
+			// bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
+			bucket[hashIndex].head = newNode;
+		}
+		else
+		{
+			newNode->next = bucket[hashIndex].head;
+
+			bucket[hashIndex].head = newNode;
+		}
+
+		// bucket[hashIndex]의 count를 증가시킵니다.
+		bucket[hashIndex].count++;
+
+		size++;
+	}
+
 };
 
 int main()
 {
 	HashTable<const char *, int> hashtable;
 
-	cout << hashtable.hash_function("Bami’s Cinder");
+	hashtable.insert("Abyssal Mask", 3000);
+	hashtable.insert("Bami's Cinder", 1000);
+	
+	hashtable.insert("Chain Vest", 800);
 
 
 	return 0;
