50d92e29b613d6b62816cda1e829e3da2b7f2b13
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-06-19 19:44:18 +0900

    (2026.6.19) 원형 큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,89 +3,81 @@
 using namespace std;
 
 template <typename T>
-class Vector
+class Stack
 {
 private:
-	int m_size;
+	int highset;
 	int capacity;
 
 	T * container;
 public:
-	Vector()
+	Stack()
 	{
-		m_size = 0;
+		highset = -1;
 		capacity = 0;
+
 		container = nullptr;
 	}
 
 	void resize(int newSize)
 	{
-		// 1. capacity에 새로운 size 값을 저장합니다.
 		capacity = newSize;
 
-		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-		//    메모리 공간을 가리키도록 합니다.
 		T * temporary = new T[capacity];
 
-		// 3. 새로운 메모리 공간의 값을 초기화합니다.
 		for (int i = 0; i < capacity; i++)
 		{
 			temporary[i] = NULL;
 		}
 
-		// 4. 기존 배열에 있는 값을 복사해서 새로운
-		//    배열에 넣어줍니다.
-		for (int i = 0; i < size(); i++)
+		for (int i = 0; i < highset + 1; i++)
 		{
 			temporary[i] = container[i];
 		}
 
-		// 5. 기본 배열의 메모리를 해제합니다.
 		delete [ ] container;
 
-		// 6. 기존 배열을 가리키던 포인터 변수의 값을
-		//    새로운 배열의 시작 주소로 가리킵니다.
 		container = temporary;
 	}
 
-	void push_back(T data)
+	void push(T data)
 	{
 		if (capacity <= 0)
 		{
 			resize(1);
 		}
-		else if (m_size >= capacity)
+		else if (highset + 1 >= capacity)
 		{
 			resize(capacity * 2);
 		}
 
-		container[m_size++] = data;
+		container[++highset] = data;
 	}
 
-	const int & size()
+	void pop()
 	{
-		return m_size;
+		if (highset <= -1)
+		{
+			cout << "stack is empty" << endl;
+		}
+		else
+		{
+			container[highset--] = NULL;
+		}
 	}
 
-	const T & operator [] (const int & index)
-	{
-		return container[index];
-	}
-
-	~Vector()
-	{
-		delete [ ] container;
-	}
 };
 
 int main()
 { 
-	Vector<int> vector;
+	Stack<int> stack;
 
-	vector.push_back(10);
-	vector.push_back(20);
-	vector.push_back(30);
-	vector.push_back(40);
+	stack.push(10);
+	stack.push(20);
+
+	stack.pop();
+	stack.pop();
+	stack.pop();
 
 	return 0;
 }
