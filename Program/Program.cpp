8937ce7f994867f789a40b50fe5c4264ac5e5ac1
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-07-01 19:44:58 +0900

    (2026.7.1) 완전 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,8 +2,89 @@
 
 using namespace std;
 
+template <typename T>
+class Tree
+{
+private:
+	struct Node
+	{
+		T data;
+		Node * left;
+		Node * right;
+
+		Node(T data)
+		{
+			this->data = data;
+
+			left = nullptr;
+			right = nullptr;
+		}
+	};
+
+	Node * root;
+
+public:
+	Tree()
+	{
+		root = nullptr;
+	}
+
+	void insert(T data)
+	{
+		if (root == nullptr)
+		{
+			root = new Node(data);
+		}
+		else
+		{
+			Node * currentNode = root;
+
+			while (currentNode != nullptr)
+			{
+				if (data < currentNode->data)
+				{
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = new Node(data);
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else if (data > currentNode->data)
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = new Node(data);
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+				else
+				{
+					break;
+				}
+
+			}
+		}
+
+	}
+
+};
+
 int main()
 { 	
+	Tree<int> tree;
+
+	tree.insert(10);
+	tree.insert(20);
+	tree.insert(13);
+	tree.insert(22);
+	tree.insert(17);
+	tree.insert(19);
 
 	return 0;
 }
